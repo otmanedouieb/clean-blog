0dd9f459b0e09826992f329b536c158194a66ba5
otmanedouieb <douieb197@gmail.com>
2024-07-20 00:10:10 +0100

    add category to db

--- posts/create.php
+++ posts/create.php
@@ -11,13 +11,15 @@ if (isset($_POST['submit'])) {
     $path = 'images/' . basename($image);
     $user_id = $_SESSION['id'];
     $username = $_SESSION['username'];
+    $category_id = $_POST['category'];
 
-    $stmt = $conn->prepare("INSERT INTO $dbname.posts (title, subtitle, body, image, user_id, username) VALUES (:title, :subtitle, :body, :image, :user_id, :username)");
+    $stmt = $conn->prepare("INSERT INTO $dbname.posts (title, subtitle, body, category_id , image, user_id, username) VALUES (:title, :subtitle, :body,:category_id, :image, :user_id, :username)");
 
     $stmt->execute([
         'title' => $title,
         'subtitle' => $subtitle,
         'body' => $body,
+        'category_id' => $category_id,
         'image' => $image,
         'user_id' => $user_id,
         'username' => $username
@@ -46,6 +48,19 @@ if (isset($_POST['submit'])) {
     <!-- subtitle input -->
     <div class="form-outline mb-4">
         <textarea type="text" name="body" id="form2Example1" class="form-control" placeholder="body" rows="8"></textarea>
+    </div>
+    <!-- categories -->
+    <?php
+    $prepare = $conn->prepare("SELECT * FROM $dbname.categories");
+    $prepare->execute();
+    $cat = $prepare->fetchAll(PDO::FETCH_OBJ);
+    ?>
+    <div class="form-outline mb-4">
+        <select class="form-select" aria-label="Default select example" name="category">
+            <?php foreach ($cat as $c) : ?>
+                <option value="<?= $c->id ?>"><?= $c->name ?></option>
+            <?php endforeach ?>
+        </select>
     </div>
     <!-- image input -->
     <div class="form-outline mb-4">
